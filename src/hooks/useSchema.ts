916393fde5beb67e63d7ca4396b2e554a9343883
Eduard Jacko <jacko.eduard@gmail.com>
2021-05-23 22:43:31 +0100

    wip

--- src/hooks/useSchema.ts
+++ src/hooks/useSchema.ts
@@ -1,18 +1,26 @@
-import { useCallback, useMemo, useReducer } from "react";
+import { useCallback, useMemo, useReducer, useRef } from "react";
 import { SchemaActionType, schemaReducer } from "../functions";
 import { validateSchema } from "../functions/validateSchema";
 import { Schema } from "../types";
 
 export const useSchema = (initSchema: Schema) => {
   validateSchema(initSchema);
+  const viewLayerRef = useRef<HTMLDivElement | null>(null);
   const [schema, onChange] = useReducer(schemaReducer, initSchema);
   const addNode = useCallback((data: { position: [number, number] }) => {
     onChange({ type: SchemaActionType.ADD_NODE, ...data });
+  }, []);
+  const clientToLocalPosition = (clientX: number, clientY: number) => {
+    console.log(clientX);
+  };
+  const setViewLayerRef = useCallback((elem: HTMLDivElement) => {
+    viewLayerRef.current = elem;
   }, []);
   const actions = useMemo(
     () => ({
       onChange,
       addNode,
+      clientToLocalPosition,
     }),
     [addNode]
   );
