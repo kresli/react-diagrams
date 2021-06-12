8abe169e54d0c9e3968fcee77b5dd85398b7c8c2
Eduard Jacko <jacko.eduard@gmail.com>
2021-06-12 21:06:52 +0100

    removed unused

--- .docusaurus/registry.js
+++
@@ -1,5 +0,0 @@
-export default {
-  'component---theme-doc-item-178-a40': [() => import(/* webpackChunkName: 'component---theme-doc-item-178-a40' */ "@theme/DocItem"), "@theme/DocItem", require.resolveWeak("@theme/DocItem")],
-  'component---theme-doc-page-1-be-9be': [() => import(/* webpackChunkName: 'component---theme-doc-page-1-be-9be' */ "@theme/DocPage"), "@theme/DocPage", require.resolveWeak("@theme/DocPage")],
-  'content---docs-introduction-972-0fc': [() => import(/* webpackChunkName: 'content---docs-introduction-972-0fc' */ "@site/docs/introduction.mdx"), "@site/docs/introduction.mdx", require.resolveWeak("@site/docs/introduction.mdx")],
-  'versionMetadata---docs-935-739': [() => import(/* webpackChunkName: 'versionMetadata---docs-935-739' */ "~docs/default/version-current-metadata-prop-751.json"), "~docs/default/version-current-metadata-prop-751.json", require.resolveWeak("~docs/default/version-current-metadata-prop-751.json")],};
